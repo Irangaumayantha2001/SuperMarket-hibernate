e9832019010bf1a3d20304cee04a1ccf047be876
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-06-16 10:04:25 +0530

    add orderDetails  property and anotation

--- src/entity/OrderDetails.java
+++ src/entity/OrderDetails.java
@@ -1,6 +1,22 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class OrderDetails {
+
+    @Id
+    private int orderDetailId;
+
+    private int orderQty;
+    private double discount;
 
 }
