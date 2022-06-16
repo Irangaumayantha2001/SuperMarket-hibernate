ef146aee33d74aa08b6948d9c5418c80fec8f7ba
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-06-16 10:01:14 +0530

    add orders  property and anotation

--- src/entity/Orders.java
+++ src/entity/Orders.java
@@ -1,7 +1,26 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Orders {
+
+    @Id
+    private String orderId;
+
+    private LocalDate orderDate;
+    private LocalTime orderTime;
+    private double cost;
 
 
 }
