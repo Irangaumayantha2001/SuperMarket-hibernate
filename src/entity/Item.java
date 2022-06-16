389703badbf3324212b555b089bfe856070a9ddb
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-06-16 09:59:37 +0530

    add Item  property and anotation

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -1,6 +1,22 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Item {
+    @Id
+    private String itemCode;
+    private String description;
+    private String packSize;
+    private double unitPrice;
+    private int qtyOnHand;
 
 }
