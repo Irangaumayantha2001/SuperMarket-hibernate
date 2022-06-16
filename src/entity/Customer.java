42dd6cd6dda8f254d9b4179e10799b5aef993368
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-06-16 09:55:17 +0530

    add customer property and anotation

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -1,5 +1,25 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer {
+
+    @Id
+    private String customerId;
+    private String customerTitle;
+    private String customerName;
+    private String customerAddress;
+    private String customerCity;
+    private String customerProvince;
+    private String customerPostalCode;
 
 }
